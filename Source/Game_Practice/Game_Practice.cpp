8c4567865b4f78624de04289765b03ea5940172a
panchocoder <ciscobernal91@gmail.com>
2026-01-08 20:45:40 -0500

    Added SCharacter & Setup Input
    -Added Character class.
    -Added MoveForward/Turn Input

---
+++ Source/Game_Practice/Game_Practice.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Game_Practice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Game_Practice, "Game_Practice" );
---
+++ Source/Game_Practice/Game_PracticeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Game_PracticeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME_PRACTICE_API AGame_PracticeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
