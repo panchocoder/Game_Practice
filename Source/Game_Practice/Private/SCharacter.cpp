95990ce84d2e089fa1b53cfcbfb06cf305a70af2
panchocoder <ciscobernal91@gmail.com>
2026-01-29 18:31:39 -0500

    Post FIEA Game Jam 2026
    - Most of the work was done with Mark, now I am going back and adding Tom's AI system.
    - Still need to organize Mark's videos

--- Source/Game_Practice/Private/SCharacter.cpp
+++ Source/Game_Practice/Private/SCharacter.cpp
@@ -49,6 +49,8 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
 	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+
 }
 
 void ASCharacter::MoveForward(float Value)
